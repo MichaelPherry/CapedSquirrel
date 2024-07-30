da8993722ac097f96877905846d30396035cb537
Michael Perry <mj2020perry@gmail.com>
2024-07-30 14:52:34 -0400

    global working

---
+++ scripts/squirrel_global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+#store global variables here, can be accessed from all scripts/sceness
+
+var is_hooked = false
+var hook_pos = Vector2(0,0)
+var can_hook = false
+var starting_bounds = [-1640, -600, 440, 50]
+var default_zoom = Vector2(1, 1)
